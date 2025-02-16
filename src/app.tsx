7659c9741c16b7137170630f52d92a3d41e0132c
krausediego <diegoemanuelk@gmail.com>
2025-02-16 10:37:05 -0300

    feat: Add analytics.

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router";
 import { routes } from "./routes";
 import { ThemeProvider } from "./components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={routes} />
+      <Analytics />
     </ThemeProvider>
   );
 }
