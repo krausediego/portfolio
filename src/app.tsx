f23c26a0ab685471c7700f122aff8874cef1265e
krausediego <diegoemanuelk@gmail.com>
2025-02-21 11:25:44 -0300

    feat: Remove unused code.

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,12 @@
-import { RouterProvider, useLocation } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import { ThemeProvider } from "./components/theme-provider";
 import { inject } from "@vercel/analytics";
 import { HelmetProvider } from "react-helmet-async";
-import { useEffect } from "react";
 
 inject();
 
 export function App() {
-  // const location = useLocation();
-
-  // useEffect(() => {
-  //   // Isso força a atualização do Helmet ao mudar de rota
-  // }, [location.pathname]);
-
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
